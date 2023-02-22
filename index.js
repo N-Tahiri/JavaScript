8c4c00cc1a2abe8d63e367c176d158ffb9558477
N-Tahiri <126117584+N-Tahiri@users.noreply.github.com>
2023-02-23 00:30:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
-console.log(input1.value, input2.value);
 
-//This line is equivalent to 2 above. Try to search its equivalent with byID...
+//This line is equivalent to 2 above. 
 const [input3, input4] = document.querySelectorAll("input");
-console.log(input3.value, input4.value);
 
 // Store result
 const result = document.getElementById("result");
@@ -14,17 +12,14 @@ const wrapper = document.getElementById("inputs"); //Get where on which element 
 
 // Start event on that element wrapper above
 wrapper.addEventListener("input", () => {
-  console.log(input1.value, input2.value);
-  console.log(typeof input1.value, typeof input2.value);
   let a = parseFloat(input1.value),
     b = Number(input2.value); // give same result
   let sum = a + b;
   if (isNaN(sum) && input1.value != "") {
-    //alert("Please enter a valid number.");
+    //alert("Please enter a valid number."); //Uncomment to use this command!
     reset();
   } else {
     result.innerText = sum;
-    console.log(typeof sum);
   }
 });
 
@@ -34,9 +29,3 @@ function reset() {
   input2.value = 0;
   result.innerText = 0;
 }
-
-/*
-let alog = typeof input1.value,
-blog = typeof b;
-console.log(alog, blog);
-*/
